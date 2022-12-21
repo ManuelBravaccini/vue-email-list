bfed9c68abb1c2ae7c8f7235e19e1ffd04cb404d
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2022-12-21 16:53:31 +0100

    Print random mail from API

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ const { createApp } = Vue
 
 createApp({
     data() {
-        return {}
-  }
+        return {
+            randomMail: '',
+        }
+    },
+
+    methods: {
+        getRandomMail(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                console.log(response.data.response);
+                this.randomMail = response.data.response;
+            });
+        }
+    },
+
+    created() {
+        this.getRandomMail(); 
+    }
 }).mount('#app')
