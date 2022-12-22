58451f89f141d4c079bdd2e20df6519de288c447
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2022-12-22 09:48:01 +0100

    List of emails from API

--- script.js
+++ script.js
@@ -3,21 +3,19 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            randomMail: '',
+            emails: []
         }
     },
 
     methods: {
-        getRandomMail(){
+    },
+    
+    mounted() {
+        for (let i = 0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) => {
-                console.log(response.data.response);
-                this.randomMail = response.data.response;
+                this.emails.push(response.data.response);
             });
         }
-    },
-
-    created() {
-        this.getRandomMail(); 
     }
 }).mount('#app')
